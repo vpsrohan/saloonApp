29c8ca7374496380eb9176ae688f9f25499502a0
Rohan <vpsrohan@gmail.com>
2026-09-13 20:43:24 +0530

    blind server to all interfaces

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.get("/health", (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   connectDB();
   console.log("Serving running in", PORT);
 });
