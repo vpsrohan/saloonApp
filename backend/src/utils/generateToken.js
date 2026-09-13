1b1b850d16c273541c7cb1b2fc7d9d3204fcf891
Rohan <vpsrohan@gmail.com>
2026-09-13 23:45:52 +0530

    editing jwt tokenisation to prod

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = async (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
